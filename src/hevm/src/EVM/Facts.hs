d214cd8be86a2c36bf66554b7ba4890d7b550ae8
Martin Lundfall <martin.lundfall@protonmail.com>
2020-05-27 22:11:47 +0200

    refactor symbolic explorer, fixing operational monad "scripting" move the interpret script to Symbolic

--- src/hevm/src/EVM/Facts.hs
+++ src/hevm/src/EVM/Facts.hs
@@ -34,8 +34,8 @@ module EVM.Facts
   , fileToFact
   ) where
 
-import EVM          (VM, Contract, litWord)
-import EVM.Concrete (Word)
+import EVM          (VM, Contract)
+import EVM.Concrete (Word, litWord)
 import EVM          (balance, nonce, storage, bytecode, env, contracts)
 import EVM.Types    (Addr)
 
